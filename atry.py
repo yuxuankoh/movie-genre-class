64100ea30c187cc00effdccaa70a9f434bf6ff83
HiokHian <56589476+HiokHian@users.noreply.github.com>
2021-04-17 00:13:12 +0800

    changing to class

--- atry.py
+++ atry.py
@@ -10,10 +10,8 @@ import os
 import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
-from tensorflow.keras import regularizers, initializers
 from keras.models import Model
 from keras.layers import Dense, Dropout, Input, Flatten
-from keras.callbacks import EarlyStopping, ModelCheckpoint
 import tensorflow_hub as hub
 import IPython.display as display
 import matplotlib as mpl
@@ -47,59 +45,67 @@ st.markdown(
     unsafe_allow_html=True
 )
 
-densenet_model = tf.keras.applications.DenseNet169(weights="imagenet", include_top=True, input_tensor=Input(shape=(256, 256, 3)))
-combined_dense1 = Dense(512, activation='relu', kernel_initializer='he_normal')(densenet_model.layers[-2].output)
-combined_drop1 = Dropout(0.5)(combined_dense1)
-combined_dense2 = Dense(256, activation='relu', kernel_initializer='he_normal')(combined_drop1)
-combined_drop2 = Dropout(0.5)(combined_dense2)
-combined_dense3 = Dense(128, activation='relu', kernel_initializer='he_normal')(combined_drop2)
-combined_drop3 = Dropout(0.5)(combined_dense3)
-combined_output = Dense(5, activation='sigmoid', kernel_initializer='glorot_uniform', name = 'combined')(combined_drop3)
+MovieModel = MovieClassificationModel()
+
+# densenet_model = tf.keras.applications.DenseNet169(weights="imagenet", include_top=True, input_tensor=Input(shape=(256, 256, 3)))
+# combined_dense1 = Dense(512, activation='relu', kernel_initializer='he_normal')(densenet_model.layers[-2].output)
+# combined_drop1 = Dropout(0.5)(combined_dense1)
+# combined_dense2 = Dense(256, activation='relu', kernel_initializer='he_normal')(combined_drop1)
+# combined_drop2 = Dropout(0.5)(combined_dense2)
+# combined_dense3 = Dense(128, activation='relu', kernel_initializer='he_normal')(combined_drop2)
+# combined_drop3 = Dropout(0.5)(combined_dense3)
+# combined_output = Dense(5, activation='sigmoid', kernel_initializer='glorot_uniform', name = 'combined')(combined_drop3)
 
 
-# # define new model
-model = Model(inputs=densenet_model.inputs, outputs=combined_output)
-model.load_weights('best_val_f1_1504211951.h5')
+# # # define new model
+# model = Model(inputs=densenet_model.inputs, outputs=combined_output)
+# model.load_weights('best_val_f1_1504211951.h5')
 
 
-def import_and_predict2(image):
-    preds = model.predict(image)
-    st.write('Prediction Results:')
-    #TRY
-    return preds
+# def import_and_predict2(image):
+#     preds = model.predict(image)
+#     st.write('Prediction Results:')
+#     #TRY
+#     return preds
 
         
-def style_transfer (image, intended):
-    image = np.asarray(image)
-    image = image.astype(np.float32)[np.newaxis, ...] / 255.
-    #sample = tf.image.resize(sample, (256,256))
+# def style_transfer (image, intended):
+#     image = np.asarray(image)
+#     image = image.astype(np.float32)[np.newaxis, ...] / 255.
+#     #sample = tf.image.resize(sample, (256,256))
 
-    hub_module = hub.load('https://tfhub.dev/google/magenta/arbitrary-image-stylization-v1-256/2')
-    # Stylize image.
-    outputs = hub_module(tf.constant(image), tf.constant(intended))
-    stylized_image = outputs[0]
+#     hub_module = hub.load('https://tfhub.dev/google/magenta/arbitrary-image-stylization-v1-256/2')
+#     # Stylize image.
+#     outputs = hub_module(tf.constant(image), tf.constant(intended))
+#     stylized_image = outputs[0]
 
-    fig = plt.figure()
-    plt.imshow(stylized_image[0,:,:,:])
-    plt.axis('off')
-    st.pyplot(fig)
+#     fig = plt.figure()
+#     plt.imshow(stylized_image[0,:,:,:])
+#     plt.axis('off')
+#     st.pyplot(fig)
 
 st.title("""
          Classification of Movie Genre using Movie Images
          """)
 st.write("CZ1105 REP Lab Group 1- Lim Sheng Jie, Ong Hiok Hian, Koh Yu Xuan")
-st.write("Try our Image Clasification Model! Simply upload an image and it will show you the genre!")
+# st.write("Try our Image Clasification Model! Simply upload an image and it will show you the genre!")
+st.write("Evaluate how good your movie poster is at conveying the genre of your film! Simply upload your movie poster and leave the rest to us!")
 
-user_input = st.text_input("Input the most desired genre (Action / Drama/ Thriller / Horror / Comedy): ")
+# user_input = st.text_input("Input the most desired genre (Action / Drama/ Thriller / Horror / Comedy): ")
+user_input = st.text_input("Input the intended genre of your film (Action / Drama/ Thriller / Horror / Comedy): ")
+# possible_genres = ['action', 'comedy', 'drama', 'horror', 'thriller']
+
+# while user_input.lower() not in possible_genres:
+#     user_input = st.text_input("Input the intended genre of your film (Action / Drama/ Thriller / Horror / Comedy): ")
 if (user_input.lower() == 'action'):
     index = 0
-if (user_input.lower() == 'comedy'):
+elif (user_input.lower() == 'comedy'):
     index = 1
-if (user_input.lower() == 'drama'):
+elif (user_input.lower() == 'drama'):
     index = 2
-if (user_input.lower() == 'horror'):
+elif (user_input.lower() == 'horror'):
     index = 3
-if (user_input.lower() == 'thriller'):
+elif (user_input.lower() == 'thriller'):
     index = 4
     
 if (user_input):
@@ -114,7 +120,7 @@ if (user_input):
         image_resized = np.asarray(image.resize(size))
 
         #MODEL CALL
-        preds = import_and_predict2(np.expand_dims(preprocess_input(image_resized), axis=0))
+        preds = MovieModel.import_and_predict(np.expand_dims(preprocess_input(image_resized), axis=0))
 
         if (preds[0][0] > 0.5276367):
             st.write('Action') #Action
@@ -157,7 +163,7 @@ if (user_input):
                 for j in range(len(cols)):
                     with cols[j]:
                         st.write("Your poster could be mistaken for " + mapbackgenre[grad_indexes[j]])
-                        runGradCAM(model, img = image, pred_index = grad_indexes[j], alpha = 2)
+                        MovieModel.run_gradCAM(img = image, pred_index = grad_indexes[j], alpha = 2)
                 st.write("Not sure how to change it? Click on View Alternatives for quick suggestions")
             else:
                 st.write("Looks Good!")
@@ -173,7 +179,7 @@ if (user_input):
                     #st.image(horror, use_column_width=True)
                     horror1 = np.asarray(horror1)
                     horror1 = horror1.astype(np.float32)[np.newaxis, ...] / 255.
-                    style_transfer(image, horror1)
+                    MovieModel.style_transfer(image, horror1)
             #horror = tf.image.resize(horror, (256,256))
                 with col2:
                     st.write("Choice 2")
@@ -181,14 +187,14 @@ if (user_input):
                     #st.image(horror, use_column_width=True)
                     horror2 = np.asarray(horror2)
                     horror2 = horror2.astype(np.float32)[np.newaxis, ...] / 255.
-                    style_transfer(image, horror2)
+                    MovieModel.style_transfer(image, horror2)
                 with col3:
                     st.write("Choice 3")
                     horror3 = Image.open('Horror3.jpg').convert('RGB')
                     #st.image(horror, use_column_width=True)
                     horror3 = np.asarray(horror3)
                     horror3 = horror3.astype(np.float32)[np.newaxis, ...] / 255.
-                    style_transfer(image, horror3)
+                    MovieModel.style_transfer(image, horror3)
 
             elif (user_input == 'Drama'):
                 col1, col2, col3 = st.beta_columns(3)
@@ -198,21 +204,21 @@ if (user_input):
                     #st.image(horror, use_column_width=True)
                     drama1 = np.asarray(drama1)
                     drama1 = drama1.astype(np.float32)[np.newaxis, ...] / 255.
-                    style_transfer(image, drama1)
+                    MovieModel.style_transfer(image, drama1)
                 with col2:
                     st.write("Choice 2")
                     drama2 = Image.open('Drama2.jpg').convert('RGB')
                     #st.image(horror, use_column_width=True)
                     drama2 = np.asarray(drama2)
                     drama2 = drama2.astype(np.float32)[np.newaxis, ...] / 255.
-                    style_transfer(image, drama2)
+                    MovieModel.style_transfer(image, drama2)
                 with col3:
                     st.write("Choice 3")
                     drama3 = Image.open('Drama2.jpg').convert('RGB')
                     #st.image(horror, use_column_width=True)
                     drama3 = np.asarray(drama3)
                     drama3 = drama3.astype(np.float32)[np.newaxis, ...] / 255.
-                    style_transfer(image, drama3)
+                    MovieModel.style_transfer(image, drama3)
 
             elif (user_input == 'Action'):
                 col1, col2, col3 = st.beta_columns(3)
@@ -222,21 +228,21 @@ if (user_input):
                     #st.image(horror, use_column_width=True)
                     action1 = np.asarray(action1)
                     action1 = action1.astype(np.float32)[np.newaxis, ...] / 255.
-                    style_transfer(image, action1)
+                    MovieModel.style_transfer(image, action1)
                 with col2:
                     st.write("Choice 2")
                     action2 = Image.open('Action2.jpg').convert('RGB')
                     #st.image(horror, use_column_width=True)
                     action2 = np.asarray(action2)
                     action2 = action2.astype(np.float32)[np.newaxis, ...] / 255.
-                    style_transfer(image, action2)
+                    MovieModel.style_transfer(image, action2)
                 with col3:
                     st.write("Choice 3")
                     action3 = Image.open('Action3.jpg').convert('RGB')
                     #st.image(horror, use_column_width=True)
                     action3 = np.asarray(action3)
                     action3 = action3.astype(np.float32)[np.newaxis, ...] / 255.
-                    style_transfer(image, action3)
+                    MovieModel.style_transfer(image, action3)
 
             elif (user_input == 'Comedy'):
                 col1, col2, col3 = st.beta_columns(3)
@@ -246,21 +252,21 @@ if (user_input):
                     #st.image(horror, use_column_width=True)
                     comedy1 = np.asarray(comedy1)
                     comedy1 = comedy1.astype(np.float32)[np.newaxis, ...] / 255.
-                    style_transfer(image, comedy1)
+                    MovieModel.style_transfer(image, comedy1)
                 with col2:
                     st.write("Choice 2")
                     comedy2 = Image.open('Comedy2.jpg').convert('RGB')
                     #st.image(horror, use_column_width=True)
                     comedy2 = np.asarray(comedy2)
                     comedy2 = comedy2.astype(np.float32)[np.newaxis, ...] / 255.
-                    style_transfer(image, comedy2)
+                    MovieModel.style_transfer(image, comedy2)
                 with col3:
                     st.write("Choice 3")
                     comedy3 = Image.open('Comedy3.jpg').convert('RGB')
                     #st.image(horror, use_column_width=True)
                     comedy3 = np.asarray(comedy3)
                     comedy3 = comedy3.astype(np.float32)[np.newaxis, ...] / 255.
-                    style_transfer(image, comedy3)
+                    MovieModel.style_transfer(image, comedy3)
 
             elif (user_input == 'Thriller'):
                 col1, col2, col3 = st.beta_columns(3)
@@ -270,20 +276,20 @@ if (user_input):
                     #st.image(horror, use_column_width=True)
                     thriller1 = np.asarray(thriller1)
                     thriller1 = thriller1.astype(np.float32)[np.newaxis, ...] / 255.
-                    style_transfer(image, thriller1)
+                    MovieModel.style_transfer(image, thriller1)
                 with col2:
                     st.write("Choice 2")
                     thriller2 = Image.open('Thriller2.jpg').convert('RGB')
                     #st.image(horror, use_column_width=True)
                     thriller2 = np.asarray(thriller2)
                     thriller2 = thriller2.astype(np.float32)[np.newaxis, ...] / 255.
-                    style_transfer(image, thriller2)
+                    MovieModel.style_transfer(image, thriller2)
                 with col3:
                     st.write("Choice 3")
                     thriller3 = Image.open('Thriller3.jpg').convert('RGB')
                     #st.image(horror, use_column_width=True)
                     thriller3 = np.asarray(thriller3)
                     thriller3 = thriller3.astype(np.float32)[np.newaxis, ...] / 255.
-                    style_transfer(image, thriller3)
+                    MovieModel.style_transfer(image, thriller3)
 
             
--- util_functions.py
+++ util_functions.py
@@ -5,106 +5,217 @@ import keras
 import streamlit as st
 import matplotlib.cm as cm
 from keras import backend as K
+from keras.models import Model
+from keras.layers import Dense, Dropout, Input, Flatten
 from PIL import Image
-import cv2
-# class ourModel:
-#     def __init__(self, ):
-#         self.model = createBaseModel()
-#         self.gradCAMModel = createGradCAMModel()
-#         self.thresholds = {0: 0.5276367, 1: 0.5806225, 2:0.39837518, 3:0.5521859, 4:0.51436293}
-#         self.size = (256,256)
-#         self.mapBackGenre =  {0: 'Action', 1: 'Comedy', 2:'Drama', 3:'Horror', 4:'Thriller'}
-#         self.preprocess_input = keras.applications.densenet.preprocess_input
+import tensorflow_hub as hub
+import matplotlib.pyplot as plt
+class MovieClassificationModel:
+    def __init__(self):
+        self.thresholds = {0: 0.5276367, 1: 0.5806225, 2:0.39837518, 3:0.5521859, 4:0.51436293}
+        self.size = (256,256)
+        self.mapBackGenre =  {0: 'Action', 1: 'Comedy', 2:'Drama', 3:'Horror', 4:'Thriller'}
+        self.preprocess_input = keras.applications.densenet.preprocess_input
+        self.last_conv_layer_name = 'conv5_block32_2_conv'
+        self.model = self.createBaseModel()
+        self.gradCAMModel = self.createGradCAMModel(self.model)
+
+    def createBaseModel(self):
+        densenet_model = tf.keras.applications.DenseNet169(weights="imagenet", include_top=True, input_tensor=Input(shape=(256, 256, 3)))
+        combined_dense1 = Dense(512, activation='relu', kernel_initializer='he_normal')(densenet_model.layers[-2].output)
+        combined_drop1 = Dropout(0.5)(combined_dense1)
+        combined_dense2 = Dense(256, activation='relu', kernel_initializer='he_normal')(combined_drop1)
+        combined_drop2 = Dropout(0.5)(combined_dense2)
+        combined_dense3 = Dense(128, activation='relu', kernel_initializer='he_normal')(combined_drop2)
+        combined_drop3 = Dropout(0.5)(combined_dense3)
+        combined_output = Dense(5, activation='sigmoid', kernel_initializer='glorot_uniform', name = 'combined')(combined_drop3)
+        model = Model(inputs=densenet_model.inputs, outputs=combined_output)
+        model.load_weights('best_val_f1_1504211951.h5')
+        return model
+
+    def createGradCAMModel(self, model):
+        gradCAMModel = tf.keras.models.Model(
+            [self.model.inputs], [self.model.get_layer(self.last_conv_layer_name).output, self.model.output]
+        )
+        return gradCAMModel
+
+
+    def import_and_predict(self, image):
+        preds = self.model.predict(image)
+        st.write('Prediction Results:')
+        return preds
+
+
+    def style_transfer(self, image, intended):
+        image = np.asarray(image)
+        image = image.astype(np.float32)[np.newaxis, ...] / 255.
+        #sample = tf.image.resize(sample, (256,256))
+
+        hub_module = hub.load('https://tfhub.dev/google/magenta/arbitrary-image-stylization-v1-256/2')
+        # Stylize image.
+        outputs = hub_module(tf.constant(image), tf.constant(intended))
+        stylized_image = outputs[0]
+
+        fig = plt.figure()
+        plt.imshow(stylized_image[0,:,:,:])
+        plt.axis('off')
+        st.pyplot(fig)
+
+
+    def make_gradcam_heatmap(self, img_array, pred_index=None):
+        # we compute the gradient of the top predicted class for our input image
+        # with respect to the activations of the last conv layer
+        with tf.GradientTape() as tape:
+            last_conv_layer_output, preds = self.gradCAMModel(img_array)
+            if pred_index is None:
+                pred_index = tf.argmax(preds[0])
+            class_channel = preds[:, pred_index]
+
+        # This is the gradient of the output neuron (top predicted or chosen)
+        # with regard to the output feature map of the last conv layer
+        grads = tape.gradient(class_channel, last_conv_layer_output)
+
+        # This is a vector where each entry is the mean intensity of the gradient
+        # over a specific feature map channel
+        pooled_grads = tf.reduce_mean(grads, axis=(0, 1, 2))
+
+        # We multiply each channel in the feature map array
+        # by "how important this channel is" with regard to the top predicted class
+        # then sum all the channels to obtain the heatmap class activation
+        last_conv_layer_output = last_conv_layer_output[0]
+        heatmap = last_conv_layer_output @ pooled_grads[..., tf.newaxis]
+        heatmap = tf.squeeze(heatmap)
+
+        # For visualization purpose, we will also normalize the heatmap between 0 & 1
+        heatmap = tf.maximum(heatmap, 0) / tf.math.reduce_max(heatmap)
+        return heatmap.numpy()
+
+    def get_img_array(self, img_path, size):
+        # `img` is a PIL image of size 299x299
+        img = keras.preprocessing.image.load_img(img_path, target_size=size)
+        # `array` is a float32 Numpy array of shape (299, 299, 3)
+        array = keras.preprocessing.image.img_to_array(img)
+        # We add a dimension to transform our array into a "batch"
+        # of size (1, 299, 299, 3)
+        array = np.expand_dims(array, axis=0)
+        return array
+
+    def save_and_display_gradcam(self, img, heatmap, cam_path="cam.jpg", alpha=0.4):
+        # Load the original image
+        base_img = keras.preprocessing.image.img_to_array(img)
+        # Rescale heatmap to a range 0-255
+        heatmap = np.uint8(255 * heatmap)
+
+        # Use jet colormap to colorize heatmap
+        jet = cm.get_cmap("jet")
+
+        # Use RGB values of the colormap
+        jet_colors = jet(np.arange(256))[:, :3]
+        jet_heatmap = jet_colors[heatmap]
+        # Create an image with RGB colorized heatmap
+        jet_heatmap = keras.preprocessing.image.array_to_img(jet_heatmap)
+        jet_heatmap = jet_heatmap.resize((base_img.shape[1], base_img.shape[0]), Image.BICUBIC)
+        jet_heatmap = keras.preprocessing.image.img_to_array(jet_heatmap)
+        # Superimpose the heatmap on original image
+        superimposed_img = jet_heatmap * alpha + base_img
+        superimposed_img = keras.preprocessing.image.array_to_img(superimposed_img)
+        # display grad CAM
+        st.image(superimposed_img, use_column_width=True, caption = "Highlighted parts show parts that may be problematic")
+
+    def run_gradCAM(self, img, pred_index, alpha = 0.4):
+        preprocess_input = keras.applications.densenet.preprocess_input
+        # Prepare image
+        img_array = np.asarray(img.resize(self.size))
+        img_array = preprocess_input(img_array)
+        img_array_pred = np.expand_dims(img_array, axis = 0)
+        # Remove last layer's sigmoid
+        self.model.layers[-1].activation = None
+
+        # Generate class activation heatmap
+        heatmap = self.make_gradcam_heatmap(img_array_pred, pred_index=pred_index)
+        # # Display heatmap
+        self.save_and_display_gradcam(img, heatmap, alpha = alpha)
+
+
+
+# def get_img_array(img_path, size):
+#     # `img` is a PIL image of size 299x299
+#     img = keras.preprocessing.image.load_img(img_path, target_size=size)
+#     # `array` is a float32 Numpy array of shape (299, 299, 3)
+#     array = keras.preprocessing.image.img_to_array(img)
+#     # We add a dimension to transform our array into a "batch"
+#     # of size (1, 299, 299, 3)
+#     array = np.expand_dims(array, axis=0)
+#     return array
     
-#     def createBaseModel():
-#         # create model
+# def make_gradcam_heatmap(img_array, model, last_conv_layer_name, pred_index=None):
+#     # First, we create a model that maps the input image to the activations
+#     # of the last conv layer as well as the output predictions
+#     grad_model = tf.keras.models.Model(
+#         [model.inputs], [model.get_layer(last_conv_layer_name).output, model.output]
+#     )
 
-#         return model
+#     # Then, we compute the gradient of the top predicted class for our input image
+#     # with respect to the activations of the last conv layer
+#     with tf.GradientTape() as tape:
+#         last_conv_layer_output, preds = grad_model(img_array)
+#         if pred_index is None:
+#             pred_index = tf.argmax(preds[0])
+#         class_channel = preds[:, pred_index]
 
-#     def createGradCAMModel(model):
+#     # This is the gradient of the output neuron (top predicted or chosen)
+#     # with regard to the output feature map of the last conv layer
+#     grads = tape.gradient(class_channel, last_conv_layer_output)
 
-#         return gradCAMModel
+#     # This is a vector where each entry is the mean intensity of the gradient
+#     # over a specific feature map channel
+#     pooled_grads = tf.reduce_mean(grads, axis=(0, 1, 2))
 
+#     # We multiply each channel in the feature map array
+#     # by "how important this channel is" with regard to the top predicted class
+#     # then sum all the channels to obtain the heatmap class activation
+#     last_conv_layer_output = last_conv_layer_output[0]
+#     heatmap = last_conv_layer_output @ pooled_grads[..., tf.newaxis]
+#     heatmap = tf.squeeze(heatmap)
 
+#     # For visualization purpose, we will also normalize the heatmap between 0 & 1
+#     heatmap = tf.maximum(heatmap, 0) / tf.math.reduce_max(heatmap)
+#     return heatmap.numpy()
 
-def get_img_array(img_path, size):
-    # `img` is a PIL image of size 299x299
-    img = keras.preprocessing.image.load_img(img_path, target_size=size)
-    # `array` is a float32 Numpy array of shape (299, 299, 3)
-    array = keras.preprocessing.image.img_to_array(img)
-    # We add a dimension to transform our array into a "batch"
-    # of size (1, 299, 299, 3)
-    array = np.expand_dims(array, axis=0)
-    return array
-    
-def make_gradcam_heatmap(img_array, model, last_conv_layer_name, pred_index=None):
-    # First, we create a model that maps the input image to the activations
-    # of the last conv layer as well as the output predictions
-    grad_model = tf.keras.models.Model(
-        [model.inputs], [model.get_layer(last_conv_layer_name).output, model.output]
-    )
+# def save_and_display_gradcam(img, heatmap, cam_path="cam.jpg", alpha=0.4):
+#     # Load the original image
+#     base_img = keras.preprocessing.image.img_to_array(img)
+#     # Rescale heatmap to a range 0-255
+#     heatmap = np.uint8(255 * heatmap)
 
-    # Then, we compute the gradient of the top predicted class for our input image
-    # with respect to the activations of the last conv layer
-    with tf.GradientTape() as tape:
-        last_conv_layer_output, preds = grad_model(img_array)
-        if pred_index is None:
-            pred_index = tf.argmax(preds[0])
-        class_channel = preds[:, pred_index]
+#     # Use jet colormap to colorize heatmap
+#     jet = cm.get_cmap("jet")
 
-    # This is the gradient of the output neuron (top predicted or chosen)
-    # with regard to the output feature map of the last conv layer
-    grads = tape.gradient(class_channel, last_conv_layer_output)
-
-    # This is a vector where each entry is the mean intensity of the gradient
-    # over a specific feature map channel
-    pooled_grads = tf.reduce_mean(grads, axis=(0, 1, 2))
-
-    # We multiply each channel in the feature map array
-    # by "how important this channel is" with regard to the top predicted class
-    # then sum all the channels to obtain the heatmap class activation
-    last_conv_layer_output = last_conv_layer_output[0]
-    heatmap = last_conv_layer_output @ pooled_grads[..., tf.newaxis]
-    heatmap = tf.squeeze(heatmap)
-
-    # For visualization purpose, we will also normalize the heatmap between 0 & 1
-    heatmap = tf.maximum(heatmap, 0) / tf.math.reduce_max(heatmap)
-    return heatmap.numpy()
-
-def save_and_display_gradcam(img, heatmap, cam_path="cam.jpg", alpha=0.4):
-    # Load the original image
-    base_img = keras.preprocessing.image.img_to_array(img)
-    # Rescale heatmap to a range 0-255
-    heatmap = np.uint8(255 * heatmap)
-
-    # Use jet colormap to colorize heatmap
-    jet = cm.get_cmap("jet")
-
-    # Use RGB values of the colormap
-    jet_colors = jet(np.arange(256))[:, :3]
-    jet_heatmap = jet_colors[heatmap]
-    # Create an image with RGB colorized heatmap
-    jet_heatmap = keras.preprocessing.image.array_to_img(jet_heatmap)
-    jet_heatmap = jet_heatmap.resize((base_img.shape[1], base_img.shape[0]), Image.BICUBIC)
-    jet_heatmap = keras.preprocessing.image.img_to_array(jet_heatmap)
-    # Superimpose the heatmap on original image
-    superimposed_img = jet_heatmap * alpha + base_img
-    superimposed_img = keras.preprocessing.image.array_to_img(superimposed_img)
-    # display grad CAM
-    st.image(superimposed_img, use_column_width=True, caption = "Highlighted parts show parts that may be problematic")
+#     # Use RGB values of the colormap
+#     jet_colors = jet(np.arange(256))[:, :3]
+#     jet_heatmap = jet_colors[heatmap]
+#     # Create an image with RGB colorized heatmap
+#     jet_heatmap = keras.preprocessing.image.array_to_img(jet_heatmap)
+#     jet_heatmap = jet_heatmap.resize((base_img.shape[1], base_img.shape[0]), Image.BICUBIC)
+#     jet_heatmap = keras.preprocessing.image.img_to_array(jet_heatmap)
+#     # Superimpose the heatmap on original image
+#     superimposed_img = jet_heatmap * alpha + base_img
+#     superimposed_img = keras.preprocessing.image.array_to_img(superimposed_img)
+#     # display grad CAM
+#     st.image(superimposed_img, use_column_width=True, caption = "Highlighted parts show parts that may be problematic")
 
 
 
-def runGradCAM(model, img, pred_index, img_size = (256,256), last_conv_layer_name = 'conv5_block32_2_conv', alpha = 0.4):
-    preprocess_input = keras.applications.densenet.preprocess_input
-    # Prepare image
-    img_array = np.asarray(img.resize(img_size))
-    img_array = preprocess_input(img_array)
-    img_array_pred = np.expand_dims(img_array, axis = 0)
-    # Remove last layer's sigmoid
-    model.layers[-1].activation = None
+# def runGradCAM(model, img, pred_index, img_size = (256,256), last_conv_layer_name = 'conv5_block32_2_conv', alpha = 0.4):
+#     preprocess_input = keras.applications.densenet.preprocess_input
+#     # Prepare image
+#     img_array = np.asarray(img.resize(img_size))
+#     img_array = preprocess_input(img_array)
+#     img_array_pred = np.expand_dims(img_array, axis = 0)
+#     # Remove last layer's sigmoid
+#     model.layers[-1].activation = None
 
-    # Generate class activation heatmap
-    heatmap = make_gradcam_heatmap(img_array_pred, model, last_conv_layer_name, pred_index=pred_index)
-    # # Display heatmap
-    save_and_display_gradcam(img, heatmap, alpha = alpha)
+#     # Generate class activation heatmap
+#     heatmap = make_gradcam_heatmap(img_array_pred, model, last_conv_layer_name, pred_index=pred_index)
+#     # # Display heatmap
+#     save_and_display_gradcam(img, heatmap, alpha = alpha)
